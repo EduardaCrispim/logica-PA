5b4dbbbb4d672b657eb44e9a14c7c2943444247f
maria crispim <Eduardacrispim7@gmail.com>
2026-08-20 12:13:51 +0000

    atividade do dia 20 de agosto

---
+++ atividade4.py
@@ -0,0 +1,13 @@
+nome_bruto =" joao pedro da silva " 
+email_bruto =" JOAO.SILVA@Escola.COM" 
+
+nome_limpo = nome_bruto.strip().upper()
+email_limpo = email_bruto.strip().lower()
+
+print("---Exercicio 1: Nome e E-mail---")
+print(f"Nome higienizado : {nome_limpo}")
+print(f"E-mail higienizado :{e-mail_limpo}/n")
+
+
+
+
